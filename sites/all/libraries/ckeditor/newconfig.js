5d32066596b8f6ebaede060ff9a6e3fb394c2e50
Geoffrey <ggevalt@youngwritersproject.org>
2015-06-26 17:17:07 -0400

    friday clean up

--- sites/all/libraries/ckeditor/newconfig.js
+++ sites/all/libraries/ckeditor/newconfig.js
@@ -7,5 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
-};
+	//   config.enterMode = 2; //disabled <p> completely
+        config.enterMode = CKEDITOR.ENTER_BR // pressing the ENTER KEY input <br/>
+        config.shiftEnterMode = CKEDITOR.ENTER_P; //pressing the SHIFT + ENTER KEYS input <p>
+        config.autoParagraph = false; // stops automatic insertion of <p> on focus
+    };
