37fee9005c790a230eb8508daf64e2abecb259d5
Théodore Biadala <theodore@biadala.net>
2015-07-18 18:16:05 +0200

    Tag1 #153912: Annotator fix: don't show adder if user has no create permissions

---
+++ sites/all/modules/custom/ywp_annotator/js/disable-annotator-adder.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Disable Annotator annotation creation widget.
+ */
+(function (Drupal) {
+
+  "use strict";
+
+  var instances = Annotator._instances;
+
+  Drupal.behaviors.disableAnnotator = {
+    /**
+     * When people don't have permission to write annotation, make it so that
+     * selecting text is always empty, for *all* Annotator objects.
+     */
+    attach: function () {
+      for (var i = 0; i < instances.length; i++) {
+        instances[i].ignoreMouseup = true;
+      }
+    }
+  };
+
+})(Drupal);
