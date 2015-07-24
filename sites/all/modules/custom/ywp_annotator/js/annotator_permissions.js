b129cd7f29e6a5767f7f95061132f3bc03608409
Théodore Biadala <theodore@biadala.net>
2015-07-25 00:19:05 +0200

    Tag1 #153912: Better way to hide unnecessary checkboxes and restrict annotation to full page nodes

--- sites/all/modules/custom/ywp_annotator/js/annotator_permissions.js
+++ sites/all/modules/custom/ywp_annotator/js/annotator_permissions.js
@@ -16,8 +16,8 @@
       Drupal.Annotator.annotator('addPlugin', 'Permissions', {
         user: settings.annotator_permissions.user,
         permissions: settings.annotator_permissions.permissions,
-        showViewPermissionsCheckbox: false,
-        showEditPermissionsCheckbox: false,
+        showViewPermissionsCheckbox: settings.annotator_permissions.showViewPermissionsCheckbox === 1,
+        showEditPermissionsCheckbox: settings.annotator_permissions.showEditPermissionsCheckbox === 1,
         userId: function (user) {
           if (user && user.uid) {
             return user.uid;
