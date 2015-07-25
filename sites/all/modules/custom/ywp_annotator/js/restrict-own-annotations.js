d23bf10f3182f96f080f0b8fe391cbb7a61979ef
Théodore Biadala <theodore@biadala.net>
2015-07-25 02:29:57 +0200

    Tag1 #153912: Implement new permission behavior
    Important, remove 'View annotations' permission and add the newly created 'View own annotations' (at the bottom of the permission table).

---
+++ sites/all/modules/custom/ywp_annotator/js/restrict-own-annotations.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Disable Annotator annotation creation widget.
+ */
+(function (Drupal) {
+
+  "use strict";
+
+  Drupal.behaviors.annotatorStore = {
+    attach: function () {}
+  };
+
+})(Drupal);
