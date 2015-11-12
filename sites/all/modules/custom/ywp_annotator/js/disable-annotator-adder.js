3c9b5255afb0bf53a9886a07079c6bc471629df3
Théodore BIADALA <theodore@biadala.net>
2015-11-12 05:04:14 +0100

    Tag1 #169772: Fix several JS issues for anonymous users

--- sites/all/modules/custom/ywp_annotator/js/disable-annotator-adder.js
+++ sites/all/modules/custom/ywp_annotator/js/disable-annotator-adder.js
@@ -6,18 +6,20 @@
 
   "use strict";
 
-  var instances = Annotator._instances;
+  if (typeof window.Annotator === 'object') {
+    var instances = Annotator._instances;
 
-  Drupal.behaviors.disableAnnotator = {
-    /**
-     * When people don't have permission to write annotation, make it so that
-     * selecting text is always empty, for *all* Annotator objects.
-     */
-    attach: function () {
-      for (var i = 0; i < instances.length; i++) {
-        instances[i].ignoreMouseup = true;
+    Drupal.behaviors.disableAnnotator = {
+      /**
+       * When people don't have permission to write annotation, make it so that
+       * selecting text is always empty, for *all* Annotator objects.
+       */
+      attach: function () {
+        for (var i = 0; i < instances.length; i++) {
+          instances[i].ignoreMouseup = true;
+        }
       }
-    }
-  };
+    };
+  }
 
 })(Drupal);
